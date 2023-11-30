31af1efd63564f28b9ecccafcadb27d67b7d75d0
guisalmeida <guisalmeida.dev@gmail.com>
2023-11-29 22:11:33 -0300

    chore: jwt handlers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,43 @@
+import { Types } from "mongoose";
+
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 export async function hashPassword(password: string): Promise<string | null> {
   return await bcrypt.hash(password, 8);
+}
+
+type TokenPayloadParamsType = {
+  id: Types.ObjectId;
+  isAdmin: boolean;
+};
+
+export async function generateToken({
+  id,
+  isAdmin,
+}: TokenPayloadParamsType): Promise<string | undefined> {
+  const token = await jwt.sign(
+    {
+      id,
+      isAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  return token;
+}
+
+export function verifyToken(token: string) {
+  return jwt.verify(
+    token,
+    process.env.JWT_SECRET,
+    (err: Error, params: TokenPayloadParamsType) => {
+      if (err) return err;
+
+      return params;
+    }
+  );
 }
