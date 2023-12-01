b47f812e21fa75ab43ac7f344a4c60f888bffe65
guisalmeida <guisalmeida.dev@gmail.com>
2023-12-01 18:26:09 -0300

    chore: create cart routes

---
+++ src/controllers/cartController.ts
@@ -0,0 +1,66 @@
+import { HydratedDocument, Types } from "mongoose";
+import { Response, Request } from "express";
+import CartModel, { CartType } from "../models/cartModel";
+import { verifyToken } from "../utils";
+
+export async function createCart(req: Request, res: Response) {
+  const newCart: HydratedDocument<CartType> = new CartModel(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (error) {
+    res.status(500).json(error as Error);
+  }
+}
+
+export async function getCart(req: Request, res: Response) {
+  const authHeader = req.headers.authorization;
+  try {
+    const token = authHeader?.split(" ")[1] || "";
+    const { id } = verifyToken(token);
+    const dbCart = await CartModel.findOne({ userId: id });
+
+    res.status(200).json(dbCart);
+  } catch (error) {
+    res.status(500).json(error as Error);
+  }
+}
+
+export async function getAllCarts(req: Request, res: Response) {
+  const query = req.query;
+
+  try {
+    if (query && query.limit) {
+      const carts = await CartModel.find().limit(Number(query.limit));
+      return res.status(200).json(carts);
+    } else {
+      const carts = await CartModel.find();
+      return res.status(200).json(carts);
+    }
+  } catch (error) {
+    res.status(500).json(error as Error);
+  }
+}
+
+export async function updateCart(req: Request, res: Response) {
+  try {
+    const updatedCart = await CartModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (error) {
+    res.status(500).json(error as Error);
+  }
+}
+
+export async function deleteCart(req: Request, res: Response) {
+  try {
+    await CartModel.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Carrinho deletado com sucesso!" });
+  } catch (error) {
+    res.status(500).json(error as Error);
+  }
+}
--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,7 +1,19 @@
 import { Router } from "express";
+import authenticate from "../middlewares/authenticate";
+import {
+  createCart,
+  getCart,
+  updateCart,
+  deleteCart,
+  getAllCarts,
+} from "../controllers/cartController";
 
 const cartRouter = Router();
 
-
+cartRouter.get("/", authenticate, getAllCarts);
+cartRouter.post("/", authenticate, createCart);
+cartRouter.get("/:id", authenticate, getCart);
+cartRouter.put("/:id", authenticate, updateCart);
+cartRouter.delete("/:id", authenticate, deleteCart);
 
 export default cartRouter;
