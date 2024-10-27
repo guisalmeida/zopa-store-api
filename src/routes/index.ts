5dfe03a5a15c649e8705ffa34ae0bda4100366e2
guisalmeida <guisalmeida.dev@gmail.com>
2024-10-27 15:27:23 -0300

    chore: add versioning on routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,14 +10,19 @@ import imagesRouter from "./imagesRoute";
 const routes = Router();
 
 routes.get("/api", (req, res) =>
-  res.status(200).json({ message: "API running succesfully" })
+  res
+    .status(200)
+    .json({
+      message: "API running succesfully",
+      version: process.env.API_VERSION,
+    })
 );
-routes.use("/api/users", usersRouter);
-routes.use("/api/auth", authRoute);
-routes.use("/api/cart", cartRouter);
-routes.use("/api/orders", ordersRouter);
-routes.use("/api/products", productsRouter);
-routes.use("/api/checkout", paymentRouter);
-routes.use("/api/images", imagesRouter);
+routes.use(`/api/${process.env.API_VERSION}/users`, usersRouter);
+routes.use(`/api/${process.env.API_VERSION}/auth`, authRoute);
+routes.use(`/api/${process.env.API_VERSION}/cart`, cartRouter);
+routes.use(`/api/${process.env.API_VERSION}/orders`, ordersRouter);
+routes.use(`/api/${process.env.API_VERSION}/products`, productsRouter);
+routes.use(`/api/${process.env.API_VERSION}/checkout`, paymentRouter);
+routes.use(`/api/${process.env.API_VERSION}/images`, imagesRouter);
 
 export default routes;
